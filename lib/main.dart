12fdd2a798fe0679a421300ea1de06d2b6cb5317
ahmedeisa22 <eisa88667@gmail.com>
2021-12-01 17:49:50 +0200

    this is interface has information about ahmed adel

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,88 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 60.0,
+                backgroundColor: Colors.white,
+                backgroundImage: AssetImage('images/me.png'),
+              ),
+              Text(
+                'Ahmed Adel',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "pacifico",
+                ),
+              ),
+              Text(
+                'Software Engineer',
+                style: TextStyle(
+                    fontFamily: 'source',
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    letterSpacing: 2.5),
+              ),
+              SizedBox(
+                height: 25.0,
+                width: 220.0,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.5),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+02 0111 89 47 989',
+                    style: TextStyle(
+                      fontFamily: 'source',
+                      color: Colors.teal,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.5),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'eisa@gmail.com',
+                    style: TextStyle(
+                      fontFamily: 'source',
+                      color: Colors.teal,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
